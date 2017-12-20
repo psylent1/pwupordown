8cc3ba8b7857633f0fe0e45cbb603d5893134562
psylent1 <13180465+psylent1@users.noreply.github.com>
2017-12-20 12:44:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
 
-<?php
-# This function reads your DATABASE_URL configuration automatically set by Heroku
-# the return value is a string that will work with pg_connect
-function pg_connection_string() {
-  // we will fill this out next
-}
- 
-# Establish db connection
-$db = pg_connect(pg_connection_string());
-if (!$db) {
-    echo "Database connection error."
-    exit;
-}
- 
-$result = pg_query($db, "SELECT statement goes here");
-?>
+<!DOCTYPE html>
 <?php
 	if(isset($_POST['name']) && isset($_POST['host']))
 	{
@@ -34,7 +19,6 @@ $result = pg_query($db, "SELECT statement goes here");
 $page = $_SERVER['PHP_SELF'];
 $sec = "10";
 ?>
-<!DOCTYPE html>
 <html>
 <link rel="shortcut icon" type="image/x-icon" href="/logo2.ico" />
     <head>
